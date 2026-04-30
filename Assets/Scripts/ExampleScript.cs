3db04d7f519fff5c050d4fef08c30e64a477912b
SpacingKosmopan <spacingkosmopan.mapy@gmail.com>
2026-04-30 18:49:13 +0200

    Resolved confict

--- Assets/Scripts/ExampleScript.cs
+++ Assets/Scripts/ExampleScript.cs
@@ -5,5 +5,5 @@ public class ExampleScript : MonoBehaviour
     [SerializeField]
     private string msg = "Kuba tu był";
     public string msg1 = "Kacper tu jednak był";
-    public string msg1 = "f esfmoi ae miea mf jweapofu wapf to nie jest treść";
+    public string msg2 = "f esfmoi ae miea mf jweapofu wapf to nie jest treść";
 }
